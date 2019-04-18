07c35969f54d56b0765a89a8817a574732b7c03f
indiflex <indiflex1@gmail.com>
2019-04-18 15:54:05 +0900

    2019-04-18 lesson

--- ml/mnist_best.py
+++ ml/mnist_best.py
@@ -18,14 +18,18 @@ def readCsv(file, maxcnt):
 
 
 train = readCsv('./data/train.csv', 1000)   # 학습용 데이터가 많아질수록 스코어 상승!
-test = readCsv('./data/t10k.csv', 100)
+test = readCsv('./data/t10k.csv', 1000)
 
 # clf = svm.SVC(gamma='auto')
+# clf = svm.SVC(C=100, cache_size=200, class_weight=None, coef0=0.0,
+#               decision_function_shape='ovr', degree=3, gamma=0.001, kernel='rbf',
+#               max_iter=-1, probability=False, random_state=None, shrinking=True,
+#               tol=0.001, verbose=False)
 
 # find best params
 params = [
     {"C": [1, 10, 100, 1000], "kernel": ['linear']},
-    {"C": [1, 10, 100, 1000], "kernel": ['rbf'], "gamma": [0.001, 0.0001]},
+    {"C": [1, 10, 100, 1000], "kernel": ['rbf'], "gamma": [0.01, 0.001, 0.0001]},
 ]
 clf = GridSearchCV(svm.SVC(), params, n_jobs=-1, cv=3, iid=True)
 
